4adb7bc65742101bf9e45c6bfb7581fafec3b324
Willian Murayama <willianyos13@gmail.com>
2024-07-04 00:14:32 -0300

    Feat: Eventos

--- Projeto/src/projeto/Eventos.java
+++ Projeto/src/projeto/Eventos.java
@@ -18,4 +18,62 @@ public class Eventos {
     private int Partes;
     private int QuantidadeVagas;
     private float TaxaInscricao;
+
+    public Eventos(Date Data, Date Hora, int CargaHoraria, int Partes, int QuantidadeVagas, float TaxaInscricao) {
+        this.Data = Data;
+        this.Hora = Hora;
+        this.CargaHoraria = CargaHoraria;
+        this.Partes = Partes;
+        this.QuantidadeVagas = QuantidadeVagas;
+        this.TaxaInscricao = TaxaInscricao;
+    }
+
+    public Date getData() {
+        return Data;
+    }
+
+    public void setData(Date Data) {
+        this.Data = Data;
+    }
+
+    public Date getHora() {
+        return Hora;
+    }
+
+    public void setHora(Date Hora) {
+        this.Hora = Hora;
+    }
+
+    public int getCargaHoraria() {
+        return CargaHoraria;
+    }
+
+    public void setCargaHoraria(int CargaHoraria) {
+        this.CargaHoraria = CargaHoraria;
+    }
+
+    public int getPartes() {
+        return Partes;
+    }
+
+    public void setPartes(int Partes) {
+        this.Partes = Partes;
+    }
+
+    public int getQuantidadeVagas() {
+        return QuantidadeVagas;
+    }
+
+    public void setQuantidadeVagas(int QuantidadeVagas) {
+        this.QuantidadeVagas = QuantidadeVagas;
+    }
+
+    public float getTaxaInscricao() {
+        return TaxaInscricao;
+    }
+
+    public void setTaxaInscricao(float TaxaInscricao) {
+        this.TaxaInscricao = TaxaInscricao;
+    }
+
 }
